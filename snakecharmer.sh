32175feda6c9256351ec1c00008a75fbcb66b317
Kristen Wells <kristen.wells@cuanschutz.edu>
2021-08-13 17:33:42 -0600

    adding genes and extending distance

--- snakecharmer.sh
+++ snakecharmer.sh
@@ -18,7 +18,11 @@ module load bedtools
 #config_file=src/configs/ca_targets_ca_tfs_human.yaml
 #config_file=src/configs/ca_targets_ca_tfs_mouse.yaml
 #config_file=src/configs/nfat_targets_all_tfs.yaml
-config_file=src/configs/nfat_targets_nfat_tfs.yaml
+#config_file=src/configs/nfat_targets_nfat_tfs.yaml
+#config_file=src/configs/nfat_gd_gfk_targets_all_tfs.yaml
+#config_file=src/configs/nfat_gd_gfk_targets_nfat_tfs.yaml
+#config_file=src/configs/nfat_g_gd_targets_all_tfs.yaml
+config_file=src/configs/nfat_g_gd_targets_nfat_tfs.yaml
 
 # Function to run snakemake
 run_snakemake() {
